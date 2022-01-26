a5bbdf5ba1751c761c348b389d31b64a5ede9f81
Aaron Minnick <abminnick@gmail.com>
2022-01-26 14:02:26 -0800

    add Summary Attributes, add sort method option

--- EpiBot/Modules/StackOverflowSearch.cs
+++ EpiBot/Modules/StackOverflowSearch.cs
@@ -10,10 +10,17 @@ namespace EpiBot.Modules
   public class StackOverflowSearch : InteractionModuleBase<SocketInteractionContext>
   {
     [SlashCommand("stackoverflow", "Search for a question on Stackoverflow.")]
-    public async Task Search(string question)
+    public async Task Search
+    (
+      [Summary("Question / phrase to search for.")]
+      string question, 
+      [Summary("Sort method for results")]
+      [Choice("activity", "activity"), Choice("votes", "votes")]
+      string sort = "activity"
+    )
     {
       RestClient client = new RestClient("https://api.stackexchange.com/2.3/");
-      RestRequest request = new RestRequest($"search?page=1&pagesize=5&order=desc&intitle={question}&site=stackoverflow", Method.Get);
+      RestRequest request = new RestRequest($"search?page=1&pagesize=5&order=desc&sort={sort}&intitle={question}&site=stackoverflow", Method.Get);
       RestResponse response = await client.ExecuteGetAsync(request);
       await RespondAsync(
         text: $"Here are some stackoverflow results similar to \"{question}\":",
