68e64ac49705a92661b63c336e63c208dfc2cf72
Jeff Terrell <jeffaterrell@gmail.com>
2022-01-26 23:30:58 -0500

    add Purge.cs command module
    Co-Authored-By: Kate Kiatsiri <keidsiri@gmail.com>

---
+++ EpiBot/Modules/Purge.cs
@@ -0,0 +1,31 @@
+using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using DiscordBot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+  public class PurgeModule : InteractionModuleBase<SocketInteractionContext>
+  {
+    [SlashCommand("purge", "Delete massages from the most recent based on number given")]
+    public async Task Purge(int number)
+    {
+      if ( number == 0)
+      {
+        await RespondAsync("No messages have been deleted!!");
+      }
+      else
+      {
+        var messages = await Context.Channel.GetMessagesAsync(number).FlattenAsync();
+        await (Context.Channel as SocketTextChannel).DeleteMessagesAsync(messages);
+        await RespondAsync($"{messages.Count()} messages deleted");
+      }
+    } 
+  } 
+}
