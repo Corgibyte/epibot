133de06f902ae0c4c2c67474190bc42c6bd588a5
Jeff Terrell <jeffaterrell@gmail.com>
2022-01-27 11:51:53 -0500

    update Migrations with new model class

---
+++ EpiBot/Migrations/20220127165128_newFeatures.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Epibot.Migrations
+{
+    public partial class newFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
