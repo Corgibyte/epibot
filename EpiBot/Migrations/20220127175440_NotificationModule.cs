6bb7f38e6e8459a7afece605a20a471b5afa398b
Colt Thatcher <colt.thatcher303@gmail.com>
2022-01-27 10:29:32 -0800

    Implement pet notifications

---
+++ EpiBot/Migrations/20220127175440_NotificationModule.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Epibot.Migrations
+{
+    public partial class NotificationModule : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PetNotificationClients",
+                columns: table => new
+                {
+                    PetNotificationClientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<ulong>(type: "bigint unsigned", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetNotificationClients", x => x.PetNotificationClientId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PetNotificationClients");
+        }
+    }
+}
---
+++ EpiBot/Models/PetNotificationClient.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EpiBot.Models
+{ 
+  public class PetNotificationClient
+  {
+    public int PetNotificationClientId { get; set; }
+
+    public ulong UserId { get; set; }
+  }
+}
